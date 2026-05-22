a6a107ee331af91dccbd8f47289fa0d9c0f8eaae
ArchmagosOfNanami <kimbraga42@gmail.com>
2026-05-22 10:01:47 +0800

    Api Integrations and issue fixes
    added api comm into the database for product CRUD

--- WarehouseAssignment2/UpdateProduct.cs
+++ WarehouseAssignment2/UpdateProduct.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
+using Newtonsoft.Json;
 
 namespace WarehouseAssignment2
 {
@@ -17,28 +12,89 @@ namespace WarehouseAssignment2
         {
             InitializeComponent();
         }
-        SqlConnection con;
-        SqlDataAdapter da;
-        DataSet ds;
-        SqlCommandBuilder cmd;
-        private void UpdateProduct_Load(object sender, EventArgs e)
+
+        private async void UpdateProduct_Load(object sender, EventArgs e)
         {
-            con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\aa\source\repos\WarehouseAssignment2\WarehouseAssignment2\Database1.mdf;Integrated Security=True");
-            con.Open();
-            da = new SqlDataAdapter("select * from Item", con);
+            try
+            {
+                var fields = new Dictionary<string, string>
+                {
+                    { "action", "list_products" }
+                };
 
-            ds = new System.Data.DataSet();
-            da.Fill(ds, "Item");
-            dataGridView1.DataSource = ds.Tables[0];
+                var rows = await ApiHelper.Post<List<Dictionary<string, object>>>(fields);
 
+                if (rows != null && rows.Count > 0)
+                {
+                    DataTable dt = new DataTable();
+                    foreach (var key in rows[0].Keys)
+                        dt.Columns.Add(key);
+
+                    foreach (var row in rows)
+                    {
+                        var dr = dt.NewRow();
+                        foreach (var key in row.Keys)
+                            dr[key] = row[key];
+                        dt.Rows.Add(dr);
+                    }
+
+                    dataGridView1.AutoGenerateColumns = true;
+                    dataGridView1.DataSource = dt;
+                }
+                else
+                {
+                    MessageBox.Show("No products found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading products: " + ex.Message);
+            }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
-            cmd = new SqlCommandBuilder(da);
-            da.Update(ds, "Item");
-            MessageBox.Show("Update Successfull");
-            
+            try
+            {
+                if (dataGridView1.CurrentRow == null)
+                {
+                    MessageBox.Show("Select a product row to update.");
+                    return;
+                }
+
+                var row = ((DataRowView)dataGridView1.CurrentRow.DataBoundItem).Row;
+
+                var fields = new Dictionary<string, string>
+                {
+                    { "action", "update_product" },
+                    { "pdt_id", row["pdt_id"].ToString() },
+                    { "pdt_name", row["pdt_name"].ToString() },
+                    { "pdt_price", row["pdt_price"].ToString() },
+                    { "pdt_des", row["pdt_des"].ToString() },
+                    { "pdt_ctg", row["pdt_ctg"].ToString() },
+                    { "pdt_stock", row["product_stock"].ToString() },
+                    { "pdt_status", row["pdt_status"].ToString() }
+                };
+
+                var result = await ApiHelper.Post<Dictionary<string, string>>(fields);
+
+                if (result.ContainsKey("message"))
+                {
+                    MessageBox.Show(result["message"]);
+                }
+                else if (result.ContainsKey("error"))
+                {
+                    MessageBox.Show("Error: " + result["error"]);
+                }
+                else
+                {
+                    MessageBox.Show("Update failed.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error updating product: " + ex.Message);
+            }
         }
     }
 }
